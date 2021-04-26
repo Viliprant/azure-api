593a30a75f422b36759561e8a739f45ca9008d60
Viliprant <viliprant@gmail.com>
2021-04-26 21:11:51 +0200

    test6

--- Routes/construction.js
+++ Routes/construction.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
-require('dotenv').config()
 
 const ConstructionModel = require('../Schemas/construction');
 
